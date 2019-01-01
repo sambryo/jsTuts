97ac665842da22ce30450b656dd8d3d22af931c3
samTer <samuelbryo@gmail.com>
2018-12-31 23:51:54 -0800

    using another object constructor to create for a new object

--- prototype.js
+++ prototype.js
@@ -17,7 +17,33 @@ Person.prototype.calculateAge = (dob) => {
     return Math.abs(age.getUTCFullYear() - 1970)
 }
 
+function Person2 (firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    this.showName = () =>{
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+function Customer(firstName, lastName, phone, membership){
+    Person2.call(this, firstName, lastName);
+    this.phone = phone;
+    this.membership = membership;
+}
 
 
+
+const john = new Customer('John', 'Paul', '444-444-4444', 'Standard')
+console.log(john);
+/* Output
+Customer {
+firstName: 'John',
+lastName: 'Paul',
+showName: [Function],
+phone: '444-444-4444',
+membership: 'Standard' }
+*/
+console.log(john.showName())
 const smith = new Person("Smith", "John")
 console.log(smith.calculateAge('05-12-1991'));
